5c21c5ce0c9a35cf8ff4e4f93dbec838a293b979
wndeld777 <wndeld777@gmail.com>
2021-10-13 10:35:46 +0900

    todo

--- react-todo-v3-a/src/comps/Buttons/GoogleButton.jsx
+++ react-todo-v3-a/src/comps/Buttons/GoogleButton.jsx
@@ -8,12 +8,10 @@ function GoogleButton() {
 
   const googleResponse = (result) => {
     const profile = result.getBasicProfile();
-
     const email = profile.getEmail();
     const id = profile.getId();
     const name = profile.getName();
     const image = profile.getImageUrl();
-
     const Token = result.getAuthResponse().id_token;
 
     setUser({
@@ -24,7 +22,7 @@ function GoogleButton() {
   };
 
   /**
-   * public/index.html 파일에 script 를 import 한다
+   * public/index.html 파일에 script를 import 한다
    * src="https://apis.google.com/js/api:client.js"
    */
   const googleSDK_init = () => {
@@ -34,9 +32,16 @@ function GoogleButton() {
     }
 
     // google API 가 활성화되고
-    // 활성화된 API 중에서 auth2가 loading(사용할 준비가 되면)
+    // 활성화된 API중에서 auth2가 loading(사용할 준비가 되면)
+    // gapi : google cloud service 를 JS에서 사용하기 위한 객체 도구
+    // gapi의 load() 함수를 사용하여 auth2 객체를 초기화하기
+    // 이때 google로 부터 부여받은 client_id를 입력한다
     window.gapi.load("auth2", async () => {
-      const auth2 = window.gapi.auth2.init({
+      // load() 함수에 의해서 auth2 객체가 초기화된다
+      // auth2 객체 : google cloud service  를 사용한 oAuth2 인증 시스템에
+      // 접근할수 있는 객체
+      // google로 로그인을 구현하기 위한 초기화 절차
+      const auth2 = await window.gapi.auth2.init({
         client_id:
           "232719777758-soudq3dk3c9qicanv8nqtnpi9srmdmq2.apps.googleusercontent.com",
         scope: "profile email",
@@ -47,16 +52,35 @@ function GoogleButton() {
         // 원하는 곳으로 redirect
       }
 
-      auth2.attachClickHandler(buttonRef.current, {}, googleResponse);
+      // 버튼을 클릭했을때
+      // google 로그인 창이 뜨도록 하는 click event 핸들러 설정
+      // buttonRef 가 ref로 설정된 컴포넌트(button 등)에
+      // 클릭 이벤트를 설정하고
+      // 해당하는 컴포넌트를 클릭하면 google 로그인 창이 뜨도록 설정하기
+      // google login 창이 popup 되고, id 를 선택하여,
+      // 정상적으로 로그인이 수행되면
+      // 로그인 이후 작동되는 callback 함수를 3번째 매개변수로 설정한다
+      // google login 창이 popup 된후 그냥 창을 닫거나
+      // 또는 정상적으로 로그인이 수행되지 않았을때 실행되는 함수를
+      // 4번째 매개변수로 설정한다
+      // 1번째 매개변수 : 누구를 클릭했을때
+      // 2번째 매개변수 : 옵션
+      // 3번째 매개변수 : 로그인이 성공했을때 실행할 함수
+      // 4번째 매개변수 : 로그인이 실패했을때 실행할 함수
+      await auth2.attachClickHandler(
+        buttonRef.current,
+        {},
+        googleResponse,
+        (err) => alert(JSON.stringify(err))
+      );
     });
   };
-
-  useEffect(googleSDK_init, []);
+  useEffect(googleSDK_init, [googleSDK_init]);
 
   const logout = (e) => {
     const auth2 = window.gapi.auth2.getAuthInstance();
     auth2?.disconnect();
-    alert("Logout OK!!!");
+    alert("LogOut OK!!!");
   };
 
   return (
